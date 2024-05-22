035b67ecd9f9b22395fdc7ac695002ee25348749
Franz <franzj.pretorius.2003@gmail.com>
2024-05-22 21:38:18 +0200

    Finalized input according to user stories

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ import { subscribe, dispatch, getState } from '/store.js'
 import { add, subtract, reset } from '/actions.js'
 
 console.log(getState())
+
+dispatch(add(getState()))
 dispatch(add(getState()))
 console.log(getState())
 
-dispatch(reset(getState()))
+dispatch(subtract(getState()))
 console.log(getState())
 
-dispatch(subtract(getState()))
+dispatch(reset(getState()))
 console.log(getState())
